1471563abd56b95d8bf46c59246aff14eed12329
Carolyn Zhang <carolyn.zhang18@gmail.com>
2024-02-17 03:36:43 -0500

    Set up backend

--- server/main.py
+++ server/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
-import cv2
-import face_recognition
-import firebase_admin
-from firebase_admin import credentials, firestore
+# import cv2
+# import face_recognition
+# import firebase_admin
+# from firebase_admin import credentials, firestore
 
+app = Flask(__name__)
 
-
-
+@app.route("/")
+def test():
+  return "hello"
