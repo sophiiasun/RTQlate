86bf58bb98b60a2fc6f4e6e693651b3148d425fe
roskzhu <roskenzhu@gmail.com>
2024-02-17 02:57:02 -0500

    firebase auth update

--- app/src/pages/auth.js
+++ app/src/pages/auth.js
@@ -4,11 +4,11 @@ import { getAnalytics } from "firebase/analytics";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCgF_Fes6vbYraGF2CoTP0bAmHqS0Xd548",
+  apiKey: "AIzaSyA8ZG7FvSF8_u3smNVX4RoWlhl8Z-weg1g",
   authDomain: "empa-b8f4b.firebaseapp.com",
   projectId: "empa-b8f4b",
   storageBucket: "empa-b8f4b.appspot.com",
-  messagingSenderId: "490983780397",
+  messagingSenderId: "748440507758",
   appId: "1:748440507758:web:275205f61f3c4c4b399d7f",
   measurementId: "G-MB182TVZ7H"
 };
