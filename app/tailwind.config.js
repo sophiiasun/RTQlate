a4c4bc96d6bc33f46684bf50fd085839f9f74958
roskzhu <roskenzhu@gmail.com>
2024-02-17 10:20:26 -0500

    tailwind fix

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: 'jit',
+  content: [
+    "./app/**/*.{html,js,jsx,ts,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
